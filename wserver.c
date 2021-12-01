bd033e6719ab4cd9465b8641cec00327f4ca78f0
Jason Han <jjhan@vassar.edu>
2021-12-01 08:28:26 -0500

    move main thread back to main()

--- wserver.c
+++ wserver.c
@@ -14,15 +14,14 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *handle_connection(void *arg) {
     sleep(5);
-    
-    while (1) {
+
+    while (true) {
         pthread_mutex_lock(&mutex);
         while (pq.size == 0)
             pthread_cond_wait(&arrived, &mutex);
         struct request request = get(&fifo);
         struct request request2 = dequeue(&pq);
-        printf("%s size: %lld\n", request2.filename,
-               request2.sbuf.st_size);
+        printf("%s size: %lld\n", request2.filename, request2.sbuf.st_size);
         pthread_mutex_unlock(&mutex);
 
         // sleep(3); // TODO: remove; tests multithreading
@@ -32,34 +31,6 @@ void *handle_connection(void *arg) {
         close_or_die(request.fd);
 
         pthread_cond_signal(&used);
-
-        pthread_mutex_unlock(&mutex);
-    }
-    return NULL;
-}
-
-void *connect_thread(void *arg) {
-    int port = *((int *) arg);
-    int listen_fd = open_listen_fd_or_die(port);
-    while (1) {
-        pthread_mutex_lock(&mutex);
-        struct sockaddr_in client_addr;
-        int client_len = sizeof(client_addr);
-        pthread_mutex_unlock(&mutex);
-
-        int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr,
-                                    (socklen_t *) &client_len);
-
-        pthread_mutex_lock(&mutex);
-        while (pq.size == MAXREQ)
-            pthread_cond_wait(&used, &mutex);
-        
-        struct request request;
-        request_parse(conn_fd, &request);
-        put(&fifo, request);
-        enqueue(&pq, request);
-        
-        pthread_cond_broadcast(&arrived);
 
         pthread_mutex_unlock(&mutex);
     }
@@ -83,22 +54,39 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "usage: wserver [-d basedir] [-p port]\n");
             exit(1);
         }
-
-    // run out of this directory
-    chdir_or_die(root_dir);
     
+    chdir_or_die(root_dir);
+
     init_fifo(&fifo);
     init_priority_queue(&pq);
-
-    pthread_t manager;
-    pthread_create(&manager, NULL, connect_thread, &port);
 
     pthread_t workers[MAXREQ];
     for (int i = 0; i < MAXREQ; i++)
         pthread_create(&workers[i], NULL, handle_connection, NULL);
 
-    for (int i = 0; i < MAXREQ; i++)
-        pthread_join(workers[i], NULL);
+    int listen_fd = open_listen_fd_or_die(port);
+    while (true) {
+        pthread_mutex_lock(&mutex);
+        struct sockaddr_in client_addr;
+        int client_len = sizeof(client_addr);
+        pthread_mutex_unlock(&mutex);
+
+        int conn_fd = accept_or_die(listen_fd, (sockaddr_t *) &client_addr,
+                                    (socklen_t *) &client_len);
+
+        pthread_mutex_lock(&mutex);
+        while (pq.size == MAXREQ)
+            pthread_cond_wait(&used, &mutex);
+
+        struct request request;
+        request_parse(conn_fd, &request);
+        put(&fifo, request);
+        enqueue(&pq, request);
+
+        pthread_cond_broadcast(&arrived);
+
+        pthread_mutex_unlock(&mutex);
+    }
 
     return 0;
 }
